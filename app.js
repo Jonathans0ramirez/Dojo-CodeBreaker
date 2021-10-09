8585685f1b47a7f6e6207ec2baa9d6ae65ff69eb
jonathans0ramirez <jonathan.ramirez@experimentality.co>
2021-10-09 10:02:09 -0500

    cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const codeBreakerRoute = require('./routes/codeBreakerRoute')
 const express = require("express")
+const cors = require("cors");
 
 const app = express()
 
+app.use(cors())
 app.use('/api/codebreaker', codeBreakerRoute)
 
 module.exports = app
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-const cors = require("cors")
 const app = require("./app");
 
 const PORT = process.env.PORT ? process.env.PORT : 8080
-
-app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${ PORT }!`);
